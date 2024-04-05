7d0bc889046649f7cc710567e4c767821ab4be66
Ruletk <damir.timergazin@gmail.com>
2024-04-05 18:13:29 +0500

    Solved first problem. Using recursion algorithm for finding minimum of the array. Wrapper method with user input for startup the algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import tasks.FirstProblem;
+
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FirstProblem.solve(scanner);
     }
 }
---
+++ src/tasks/FirstProblem.java
@@ -0,0 +1,39 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class FirstProblem {
+    /**
+     * This method calculates the minimum elements of the array.
+     * Time complexity of the algorithm is O(n)
+     * This function recursively finds the minimum element in the array.
+     * When index is greater than or equal than n, method will return
+     *
+     * @param n     Count of elements in array
+     * @param array Array of numbers
+     * @return Minimum element of the array
+     */
+    public static int minElement(int n, int[] array, int index) {
+        return index < n ? Math.min(array[index], minElement(n, array, index + 1)) : Integer.MAX_VALUE;
+    }
+
+    /**
+     * Wrapper function for algorithm.
+     * This function reads input from user.
+     * It reads first number n: count of elements
+     * Then it reads every number from user input and writes it to array.
+     *
+     * @param scanner Scanner object for user input
+     */
+    public static void solve(Scanner scanner) {
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int solution = FirstProblem.minElement(n, array, 0);
+        System.out.println(solution);
+    }
+}
